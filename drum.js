b531c45fa9d88d082fce4bf5fec3d8b0aa1fc60a
EasierByCode <Daniel@EasierByCode.com>
2022-01-19 13:54:44 -0700

    update drum

--- drum.js
+++ drum.js
@@ -1,8 +1,18 @@
-window.addEventListener('keydown', e => {
+function playSound( e ) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if ( !audio )  return;  // stop here if no audio
     audio.currentTime = 0;
     audio.play();
-    key.classList.add('playing');
-})
+    key.classList.add('playing'); 
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; 
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener( 'keydown', playSound );
