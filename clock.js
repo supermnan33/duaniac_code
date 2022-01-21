0ebf6fc8844f8bf46bf295998296a2121fb76ff9
EasierByCode <Daniel@EasierByCode.com>
2022-01-21 13:51:56 -0700

    add clock

--- clock.js
+++ clock.js
@@ -2,10 +2,12 @@
 const secondHand = document.getElementsByClassName('second-hand')[0]
 const minuteHand = document.getElementsByClassName('min-hand')[0]
 const hourHand = document.getElementsByClassName('min-hand')[0]
+const timeText = document.getElementsByClassName('time')[0]
+let second = new Date().getSeconds()
 
 
 function currentSecond() {
-  const second = new Date().getSeconds()
+  second = new Date().getSeconds()
   const degreeOfCurrentSecond = ((second / 60) * 360) + 90
   secondHand.style.transform = `rotate(${degreeOfCurrentSecond}deg)`
 
@@ -16,9 +18,18 @@ function currentSecond() {
   const hour = new Date().getHours()
   const degreeOfCurrentHour = ((hour / 12) * 360) + 90
   hourHand.style.transform = `rotate(${degreeOfCurrentHour}deg)`
+
+  updateMinuteOnChange()
 }
 
 setInterval(
   currentSecond,
   1000
 )
+
+function updateMinuteOnChange() {
+
+  if ( second === 0 ) {
+    timeText.innerHTML = new Date().toLocaleTimeString().split(/\:\d{2}\s/)[0]
+  }
+}
